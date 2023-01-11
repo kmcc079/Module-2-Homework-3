7500720cacb4bba77bbe57bb1da4c931e1aee44f
Kati McConaghy <kmcc079@gmail.com>
2023-01-11 11:31:02 -0500

    first commit

---
+++ hw3_mod.py
@@ -0,0 +1,9 @@
+def area(length, width):
+    area = length * width
+    return f"The house is {area} square feet."
+
+
+def circumference(radius):
+    PI = 3.14
+    circumference = 2 * PI * radius
+    return f"The circumference of the circle is approximately {circumference}."
